aac240b55654fb646f64217f5a93316f54a9240f
Uthila Wickramage <uhwickramage@gmail.com>
2024-06-12 21:47:13 +0530

    Queue Class Created

--- src/com/jiat/wickramage_uthila/Queue.java
+++ src/com/jiat/wickramage_uthila/Queue.java
@@ -1,5 +1,31 @@
 package com.jiat.wickramage_uthila;
 
 public class Queue {
+    public int[] data;
+    public int FRONT;
+    public int REAR;
+    public int size;
+    public int capacity;
 
+    public Queue(int capacity) {
+        this.capacity = capacity;
+        this.FRONT = -1;
+        this.REAR = -1;
+        this.data = new int[capacity];
+    }
+
+    public boolean isFull(){
+        return REAR==capacity-1;
+    }
+
+    public boolean isEmpty(){
+        return REAR==-1;
+    }
+    public void enqueue(int value){
+if(!isFull()){
+    if(!isEmpty()){
+
+    }
+}
+    }
 }
--- src/com/jiat/wickramage_uthila/Test.java
+++ src/com/jiat/wickramage_uthila/Test.java
@@ -1,5 +1,8 @@
 package com.jiat.wickramage_uthila;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class Test {
     public static void main(String[] args) {
 
